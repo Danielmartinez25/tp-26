0b6946f1b88d834c8730e7fd1cf1544a77631884
Danimartinez25 <danimartinez1325@gmail.com>
2022-11-08 01:24:38 -0300

    tp-26

---
+++ public/js/index.js
@@ -0,0 +1,53 @@
+window.onload = function () {
+  //JavaScript del Index
+  let container = document.querySelector(".container");
+  let subtitulo = document.querySelector(".subtitulo");
+  let destacado = document.querySelectorAll("p");
+  let fondo = document.querySelector("body");
+  let enlace = document.querySelector("a");
+  let menu = document.getElementById("menu");
+  let logoDH = document.querySelector(".logoDH");
+
+  let nombre = "Daniel";
+  console.log(nombre);
+  if (true) {
+    subtitulo.innerHTML += ` ${nombre}`;
+  } else {
+    subtitulo.innerHTML += "INVITADO";
+  }
+
+  subtitulo.style.textTransform = "uppercase";
+  let confirmar = true;
+/*   if (confirmar) {
+    fondo.classList.add("fondo");
+    enlace.style.color = "#E51B3E";
+  } */
+  console.log(destacado);
+  for (let i = 0; i < destacado.length; i++) {
+    if (i % 2 == 0) {
+      destacado[i].classList.add("destacadoPar");
+    } else {
+      destacado[i].classList.add("destacadoImpar");
+    }
+  }
+
+  container.style.display = "block";
+
+  logoDH.addEventListener("click", () => {
+    menu.classList.toggle("mostrar");
+  });
+  /*     document.getElementById('link-list').addEventListener('click',(e)=>{
+        e.preventDefault()
+        let respuesta = confirm('estas seguro?')
+        if(respuesta){
+            window.location.href = e.target.href
+        }
+    }) */
+      menu.addEventListener("mouseout", () => {
+        menu.classList.toggle("mostrar");
+      });
+  logoDH.addEventListener("mouseover", () => {
+    fondo.classList.add("fondo");
+    enlace.style.color = "#E51B3E";
+  });
+};
